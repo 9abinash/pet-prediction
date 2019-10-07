35a772b3f591a8d29bbee79b2ff77d1a7c9658a9
9abinash <9abinash@gmail.com>
2019-10-07 14:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from keras.models import load_model
+
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
+model = load_model('model_new.h5')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+# @app.route('/predict',methods=['POST'])
+# def predict():
+    # '''
+    # For rendering results on HTML GUI
+    # '''
+    # int_features = [int(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+    # prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+
+    # return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+	
+	
+@app.route("/predict", methods=["POST"])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(224, 224))
+    
+    prediction = model.predict(processed_image).tolist()
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    response = {
+        'prediction': {
+            'dog': prediction[0][0],
+            'cat': prediction[0][1]
+        }
+    }
+    # jsonify(response)
+	return render_template('index.html', prediction_text='The pic you uploaded $ {}'.format(output))
 
 
 if __name__ == "__main__":
