435313314f3e2c1e09584a5fe4e0cc57b20af0da
9abinash <9abinash@gmail.com>
2019-10-07 16:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,38 +12,38 @@ model = load_model('model_new.h5')
 def home():
     return render_template('index.html')
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     '''
-#     For rendering results on HTML GUI
-#     '''
-#     int_features = [int(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)]
-#     prediction = model.predict(final_features)
-
-#     output = round(prediction[0], 2)
-
-#     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-	
-	
-@app.route("/predict", methods=["POST"])
+@app.route('/predict',methods=['POST'])
 def predict():
-    message = request.get_json(force=True)
-    encoded = message['image']
-    decoded = base64.b64decode(encoded)
-    image = Image.open(io.BytesIO(decoded))
-    processed_image = preprocess_image(image, target_size=(224, 224))
-    
-    prediction = model.predict(processed_image).tolist()
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-    response = {
-        'prediction': {
-            'dog': prediction[0][0],
-            'cat': prediction[0][1]
-        }
-    }
-    #output=jsonify(response)
-    return render_template('index.html', prediction_text='The pic you uploaded $ {}'.format(response))
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+	
+	
+# @app.route("/predict", methods=["POST"])
+# def predict():
+    # message = request.get_json(force=True)
+    # encoded = message['image']
+    # decoded = base64.b64decode(encoded)
+    # image = Image.open(io.BytesIO(decoded))
+    # processed_image = preprocess_image(image, target_size=(224, 224))
+    
+    # prediction = model.predict(processed_image).tolist()
+
+    # response = {
+        # 'prediction': {
+            # 'dog': prediction[0][0],
+            # 'cat': prediction[0][1]
+        # }
+    # }
+    output=jsonify(response)
+    # return render_template('index.html', prediction_text='The pic you uploaded $ {}'.format(response))
 
 if __name__ == "__main__":
     app.run(debug=True)
